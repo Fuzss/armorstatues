28b0e555a4d7c6113b37dec65391babc301694e5
Fuzss <28218241+Fuzss@users.noreply.github.com>
2023-08-16 17:54:19 +0200

    fix forge crash when mod is missing on server

--- 1.20/CHANGELOG.md
+++ 1.20/CHANGELOG.md
@@ -3,6 +3,10 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog].
 
+## [v8.0.3-1.20.1] - 2023-08-16
+### Fixed
+- Fixed a crash trying to interact with an armor stand when playing on a Forge server that does not have Armor Statues installed (Forge only)
+
 ## [v8.0.2-1.20.1] - 2023-07-29
 ### Fixed
 - Fixed an issue with always receiving an incorrect error message when trying to edit an armor stand on a server with the Vanilla Tweaks data pack installed
--- 1.20/Common/src/main/java/fuzs/armorstatues/handler/ArmorStandInteractHandler.java
+++ 1.20/Common/src/main/java/fuzs/armorstatues/handler/ArmorStandInteractHandler.java
@@ -16,6 +16,7 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.decoration.ArmorStand;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 
@@ -24,8 +25,11 @@ public class ArmorStandInteractHandler {
 
     public static EventResultHolder<InteractionResult> onUseEntityAt(Player player, Level level, InteractionHand interactionHand, Entity target, Vec3 hitVector) {
         if (!player.isSpectator() && target.getType() == EntityType.ARMOR_STAND) {
-            EventResultHolder<InteractionResult> result = ArmorStandInteractHelper.tryOpenArmorStatueMenu(player, level, interactionHand, (ArmorStand) target, ModRegistry.ARMOR_STAND_MENU_TYPE.get(), ModRegistry.ARMOR_STAND_DATA_PROVIDER);
-            if (result.isInterrupt() && level.isClientSide && !presentServerside) {
+            boolean clientsideOnly = level.isClientSide && !presentServerside;
+            // the menu won't exist in the registry if the mod is missing serverside since Forge syncs registries to clients
+            MenuType<?> menuType = clientsideOnly ? null : ModRegistry.ARMOR_STAND_MENU_TYPE.get();
+            EventResultHolder<InteractionResult> result = ArmorStandInteractHelper.tryOpenArmorStatueMenu(player, level, interactionHand, (ArmorStand) target, menuType, ModRegistry.ARMOR_STAND_DATA_PROVIDER);
+            if (result.isInterrupt() && clientsideOnly) {
                 Proxy.INSTANCE.openArmorStandScreen((ArmorStand) target, player);
                 // required so no packet is sent to server when only installed client-side, so the server doesn't change any equipment when we only want to open the screen
                 // returning InteractionResult.FAIL will miss out on the player arm swing animation, which we manually play here
